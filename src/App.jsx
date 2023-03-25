71c92c81016d8cb41c095f0bb63651844e41ee0e
umitcatal <umit130@gmail.com>
2023-03-26 00:29:35 +0300

    Add logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+/** @format */
+
 import { useState } from "react";
 import "./App.css";
 import { Routes, Route, Link, Outlet } from "react-router-dom";
 import { Button, Stack } from "@chakra-ui/react";
 import Login from "./compoents/SignIn/Logins";
 import Register from "./compoents/SignUp/Register";
-
-
+import Slider from "./compoents/Slider/Slider";
+import logo from './assets/etkinlik-logo.svg';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -17,9 +19,8 @@ function App() {
           <Route index element={<Home />} />
           <Route path="events" element={<Events />} />
           <Route path="*" element={<NoMatch />} />
-          <Route path="/login" element={<Login/>} />
-          <Route path="/register" element={<Register/>} />
-
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Route>
       </Routes>
     </div>
@@ -29,9 +30,8 @@ function App() {
 function Layout() {
   return (
     <div>
-      {/* A "layout route" is a good place to put markup you want to
-          share across all the pages on your site, like navigation. */}
       <nav className="nav">
+        <img src={logo} alt="" />
         <ul className="nav-links">
           <li>
             <Link to="/">Home</Link>
@@ -41,25 +41,23 @@ function Layout() {
           </li>
         </ul>
         <Stack direction="row" spacing={4}>
-        <Link to="/login">
-          <Button colorScheme="pink" variant="outline">
+          <Link to="/login">
+            <Button colorScheme="pink" variant="outline">
               Login
             </Button>
-        </Link>
+          </Link>
 
-        <Link to="/register">
-          <Button colorScheme="blue" variant="outline">
-            Register
-          </Button>
-        </Link>
-          
+          <Link to="/register">
+            <Button colorScheme="blue" variant="outline">
+              Register
+            </Button>
+          </Link>
         </Stack>
       </nav>
 
       <hr />
 
       <Outlet />
-     
     </div>
   );
 }
@@ -68,6 +66,8 @@ function Home() {
   return (
     <div>
       <h2>Home</h2>
+
+      <Slider />
     </div>
   );
 }
@@ -98,6 +98,5 @@ function NoMatch() {
     </div>
   );
 }
-
 
 export default App;
